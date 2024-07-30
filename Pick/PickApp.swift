3fb253ab41b0bed5c237c6ce780893038c54f6e3
Om Preetham <ompreethambandi@gmail.com>
2024-07-30 14:31:36 -0500

    Initial Commit

---
+++ Pick/PickApp.swift
@@ -0,0 +1,17 @@
+//
+//  PickApp.swift
+//  Pick
+//
+//  Created by Om Preetham Bandi on 7/30/24.
+//
+
+import SwiftUI
+
+@main
+struct PickApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
